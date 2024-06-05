978257c8c8d7594b27c98ae28c9c8f88f6bb30ea
Hermogeno22 <hermogenosheilamae@gmail.com>
2024-06-05 21:02:05 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,30 +1,30 @@
 const btnLike1 = document.getElementById("btnLike1")
 const btnLike2 = document.getElementById("btnLike2")
-const btndisLike1 = document.getElementById("btndisLike1")
-const btndisLike2 = document.getElementById("btndisLike2")
+const btndisLike1 = document.getElementById("btndislike1")
+const btndisLike2 = document.getElementById("btndislike2")
 const countLikes1 = document.getElementById("countLikes1")
 const countLikes2 = document.getElementById("countLikes2")
-const countdisLikes1 = document.getElementById("countdisLikes1")
-const countdisLikes2 = document.getElementById("countdisLikes2")
+const countdislikes1 = document.getElementById("countdislikes1")
+const countdislikes2 = document.getElementById("countdislikes2")
 
 
 function hitLikes1() {
   let totalLikes1 = parseInt(countLikes1.value) + 1
  countLikes1.textContent = totalLikes1.toString()
-  let totaldisLikes1 = parseInt(countdisLikes1.value) + 0
- countDisLikes1.textContent = totaldisLikes1.toString()
+  let totaldislikes1 = parseInt(countdislikes1.value) + 0
+ countDisLikes1.textContent = totaldislikes1.toString()
  }
 function hitLikes2() {
   let totalLikes2 = parseInt(countLikes2.value) + 1
  countLikes2.textContent = totalLikes2.toString()
 }
 function hitLikes2() {
-  let totaldisLikes2 = parseInt(countdisLikes2.value) + 0
- countdisLikes2.textContent = totaldisLikes2.toString()
+  let totaldislikes2 = parseInt(countdislikes2.value) + 0
+ countdisLikes2.textContent = totaldislikes2.toString()
 }
 
 btnLike1.addEventListener("click",hitLikes1)
 btnLike2.addEventListener("click",hitLikes2)
-btndisLike1.addEventListener("click",hitdisLikes1)
-btndisLike2.addEventListener("click",hitdisLikes2)
+btndisLike1.addEventListener("click",hitdislikes1)
+btndisLike2.addEventListener("click",hitdislikes2)
   
