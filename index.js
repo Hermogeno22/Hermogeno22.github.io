77c9b910f729eb70a2c27d130e13726475d61b68
Hermogeno22 <hermogenosheilamae@gmail.com>
2024-06-07 21:44:04 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,32 +1,32 @@
 const btnLike1 = document.getElementById("btnLike1")
 const btnLike2 = document.getElementById("btnLike2")
-const btndisLike1 = document.getElementById("btndisLike1")
-const btndisLike2 = document.getElementById("btndisLike2")
+const btnDisLike1 = document.getElementById("btnDisLike1")
+const btnDisLike2 = document.getElementById("btnDisLike2")
 const countLikes1 = document.getElementById("countLikes1")
 const countLikes2 = document.getElementById("countLikes2")
-const countdisLikes1 = document.getElementById("countdisLikes1")
-const countdisLikes2 = document.getElementById("countdisLikes2")
+const countDisLikes1 = document.getElementById("countDisLikes1")
+const countDisLikes2 = document.getElementById("countDisLikes2")
 
 
 function hitLikes1() {
   let totalLikes1 = parseInt(countLikes1.value) + 1
  countLikes1.textContent = totalLikes1.toString()
 }
-function hitdisLikes1() {
-  let totaldislikes1 = parseInt(countdisLikes1.value) + 1
- countDisLikes1.textContent = totaldisLikes1.toString()
+function hitDisLikes1() {
+  let totalDislikes1 = parseInt(countDisLikes1.value) + 1
+ countDisLikes1.textContent = totalDisLikes1.toString()
  }
 function hitLikes2() {
   let totalLikes2 = parseInt(countLikes2.value) + 1
  countLikes2.textContent = totalLikes2.toString()
 }
-function hitdisLikes2() {
-  let totaldislikes2 = parseInt(countdisLikes2.value) + 1
- countdisLikes2.textContent = totaldisLikes2.toString()
+function hitDisLikes2() {
+  let totalDislikes2 = parseInt(countDisLikes2.value) + 1
+ countDisLikes2.textContent = totalDisLikes2.toString()
 }
 
 btnLike1.addEventListener("click",hitLikes1)
 btnLike2.addEventListener("click",hitLikes2)
-btndisLike1.addEventListener("click",hitdisLikes1)
-btndisLike2.addEventListener("click",hitdisLikes2)
+btnDisLike1.addEventListener("click",hitDisLikes1)
+btnDisLike2.addEventListener("click",hitDisLikes2)
   
