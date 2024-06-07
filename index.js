958d8d2237a2bc7e65016d813b040d8285e94db6
Hermogeno22 <hermogenosheilamae@gmail.com>
2024-06-07 21:28:39 +0800

    Update index.js

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ const btndisLike1 = document.getElementById("btndisLike1")
 const btndisLike2 = document.getElementById("btndisLike2")
 const countLikes1 = document.getElementById("countLikes1")
 const countLikes2 = document.getElementById("countLikes2")
-const countdislikes1 = document.getElementById("countdisLikes1")
-const countdislikes2 = document.getElementById("countdisLikes2")
+const countdisLikes1 = document.getElementById("countdisLikes1")
+const countdisLikes2 = document.getElementById("countdisLikes2")
 
 
 function hitLikes1() {
@@ -19,6 +19,7 @@ function hitdisLikes1() {
 function hitLikes2() {
   let totalLikes2 = parseInt(countLikes2.value) + 1
  countLikes2.textContent = totalLikes2.toString()
+}
 function hitdisLikes2() {
   let totaldislikes2 = parseInt(countdisLikes2.value) + 1
  countdisLikes2.textContent = totaldisLikes2.toString()
