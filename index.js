27a23a1805eb7dbcb5683c3dd1fc36fa5546d401
Hermogeno22 <hermogenosheilamae@gmail.com>
2024-06-08 07:04:58 +0800

    Update index.js

--- index.js
+++ index.js
@@ -2,31 +2,31 @@ const btnLike1 = document.getElementById("btnLike1")
 const btnLike2 = document.getElementById("btnLike2")
 const btnDisLike1 = document.getElementById("btnDisLike1")
 const btnDisLike2 = document.getElementById("btnDisLike2")
-const countLike1 = document.getElementById("countLike1")
-const countLike2 = document.getElementById("countLike2")
-const countDisLike1 = document.getElementById("countDisLike1")
-const countDisLike2 = document.getElementById("countDisLike2")
+const countLikes1 = document.getElementById("countLikes1")
+const countLikes2 = document.getElementById("countLikes2")
+const countDisLikes1 = document.getElementById("countDisLikes1")
+const countDisLikes2 = document.getElementById("countDisLikes2")
 
 
 function hitLikes1() {
-  let totalLikes1 = parseInt(countLike1.value) + 1
- countLikes1.textContent = totalLike1.toString()
+  let totalLikes1 = parseInt(countLikes1.value) + 1
+ countLikes1.textContent = totalLikes1.toString()
 }
-function hitDisLike1() {
-  let totalDislike1 = parseInt(countDisLike1.value) + 1
- countDisLike1.textContent = totalDisLike1.toString()
+function hitDisLikes1() {
+  let totalDislikes1 = parseInt(countDisLikes1.value) + 1
+ countDisLikes1.textContent = totalDisLikes1.toString()
  }
 function hitLikes2() {
-  let totalLikes2 = parseInt(countLike2.value) + 1
- countLikes2.textContent = totalLike2.toString()
+  let totalLikes2 = parseInt(countLikes2.value) + 1
+ countLikes2.textContent = totalLikes2.toString()
 }
-function hitDisLike2() {
-  let totalDislike2 = parseInt(countDisLike2.value) + 1
- countDisLike2.textContent = totalDisLike2.toString()
+function hitDisLikes2() {
+  let totalDislikes2 = parseInt(countDisLikes2.value) + 1
+ countDisLike2.textContent = totalDisLikes2.toString()
 }
 
 btnLike1.addEventListener("click",hitLike1)
 btnLike2.addEventListener("click",hitLike2)
-btnDisLike1.addEventListener("click",hitDisLike1)
-btnDisLike2.addEventListener("click",hitDisLike2)
+btnDisLikes1.addEventListener("click",hitDisLikes1)
+btnDisLikes2.addEventListener("click",hitDisLikes2)
   
